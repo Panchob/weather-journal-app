92947e3380b5dcbe0c0823642521d9503353147a
Francis <bilodeaurobillardfrancis@gmail.com>
2020-06-11 15:03:01 -0400

    Feat: setting up the server

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-// Setup empty JS object to act as endpoint for all routes
 projectData = {};
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-// Require Express to run server and routes
+const app = express();
 
-// Start up an instance of app
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-
-// Initialize the main project folder
+app.use(cors());
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8080;
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log('server running')
+    console.log(`running on localhost: ${port}`)
+}

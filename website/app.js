6e40cacf8b3f7434d65ba0512f3eaca3d995d3d2
Francis <bilodeaurobillardfrancis@gmail.com>
2020-06-12 10:27:02 -0400

    Feat: Connection to openweather API

--- website/app.js
+++ website/app.js
@@ -1,5 +1,28 @@
-/* Global Variables */
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const key = '&appid=86bf9314a59aa62020c8d34c9cadb441';
+const country = ',us';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+document.getElementById('generate').addEventListener('click', generate);
+
+function generate(e){
+    const zip = document.getElementById('zip').value;
+    console.log(zip);
+    getWeather(baseURL, zip, country, key);
+}
+
+const getWeather = async (baseURL, zip, country, key)=>{
+    const res = await fetch(baseURL+zip+country+key)
+    
+    try {
+        const data = await res.json();
+        return data;
+
+    }
+    catch(error) {
+        console.log("error", error);
+    }
+}
